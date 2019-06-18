8cd726aa156cff6acaa2ee0f42bc531e2ef3f667
prajwalfc <prajwal_k@live.com>
2019-06-18 18:01:39 -0400

    removed a rdd

--- src/script.py
+++ src/script.py
@@ -79,8 +79,7 @@ if __name__ == "__main__":
 	rdd2 = rdd.map(lambda x: (x[1], [(x[2],x[3],x[0])]))
 	rdd3 = rdd2.reduceByKey(lambda x,y:x+y)
 	rdd4 = rdd3.map(compareComments)
-	rdd5=rdd4.filter(lambda x:x !=None).map(lambda x:x[1]).flatMap(lambda x:x)
-	df=spark.createDataFrame(rdd4.filter(lambda x:x !=None).map(lambda x:x[1]).flatMap(lambda x:x),schema=["subreddit_id","subreddit","body"])
+	df=spark.createDataFrame(rdd4.filter(lambda x:x !=None).map(lambda x:x[1]).flatMap(lambda x:x),["subreddit_id","subreddit","body"])
 	df.write.format("jdbc").options(
                 url="jdbc:postgresql://ec2-3-219-171-129.compute-1.amazonaws.com:5432/reddit",
                 dbtable="test1",
